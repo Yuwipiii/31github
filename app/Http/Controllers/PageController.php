3119c44a1a551b2133a7815f8fa309a9ef7b4bd0
Eldos Ulanbekov <ulanbekoveldos@gmail.com>
2023-12-06 17:38:35 +0600

    Change timetable

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,7 +8,13 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function index(){
-        $lessons = Lesson::all();
-        return view('index',compact('lessons'));
+        $monday = Lesson::where('day','=',1)->get();
+        $tuesday = Lesson::where('day','=',2)->get();
+        $wednesday = Lesson::where('day','=',3)->get();
+        $thursday = Lesson::where('day','=',4)->get();
+        $friday = Lesson::where('day','=',5)->get();
+        $saturday = Lesson::where('day','=',6)->get();
+        $sunday = Lesson::where('day','=',7)->get();
+        return view('index',compact('monday','thursday','tuesday','wednesday','friday','sunday','saturday'));
     }
 }
